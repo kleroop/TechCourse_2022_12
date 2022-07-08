03d4e131e0a3a745c43db788b48471d076489ba4
romoho <roman.saviuk.knm.2020@lpnu.ua>
2022-07-08 21:34:12 +0300

    styling fixes

--- client/main_window.cpp
+++ client/main_window.cpp
@@ -11,23 +11,19 @@
 #include <QByteArray>
 
 MainWindow::MainWindow(QWidget *parent)
-        : QMainWindow(parent)
-        , ui(new Ui::MainWindow)
-{
+        : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
 
-void MainWindow::loginResult(QNetworkReply *reply){
-    if(reply->error() != QNetworkReply::NoError){
+void MainWindow::loginResult(QNetworkReply *reply) {
+    if (reply->error() != QNetworkReply::NoError) {
         QString err = reply->errorString();
         QMessageBox::information(this, "Connection Error", err);
-    }
-    else{
+    } else {
         QString contents = QString::fromUtf8(reply->readAll());
         ui->tokenText->setText(contents);
         ui->stackedWidget->setCurrentIndex(1);
@@ -35,28 +31,27 @@ void MainWindow::loginResult(QNetworkReply *reply){
     reply->deleteLater();
 }
 
-void MainWindow::on_loginButton_clicked()
-{
+void MainWindow::on_loginButton_clicked() {
     QString adminEmail = ui->emailForm->text();
     QString adminPassword = ui->passwordForm->text();
 
-    QJsonObject loginbody;
-    loginbody["email"] = adminEmail;
-    loginbody["password"] = adminPassword;
-    QJsonDocument doc(loginbody);
+    QJsonObject loginBody;
+    loginBody["email"] = adminEmail;
+    loginBody["password"] = adminPassword;
+    QJsonDocument doc(loginBody);
     QByteArray data = doc.toJson();
 
     auto *manager = new QNetworkAccessManager(this);
 
-    QUrl loginpath(loginurl);
+    QUrl loginPath(loginUrl);
 
-    QNetworkRequest request(loginpath);
+    QNetworkRequest request(loginPath);
 
-    request.setHeader( QNetworkRequest::ContentTypeHeader, "application/json" );
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
 
     QNetworkReply *reply = manager->post(request, data);
 
-    QObject::connect(reply, &QNetworkReply::finished, [=](){
+    QObject::connect(reply, &QNetworkReply::finished, [=]() {
         this->loginResult(reply);
         reply->deleteLater();
     });
--- client/main_window.h
+++ client/main_window.h
@@ -1,29 +1,30 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#ifndef MAIN_WINDOW_H
+#define MAIN_WINDOW_H
 
 #include "QNetworkReply"
 #include <QMainWindow>
-#include <QUrl>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
 Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    explicit MainWindow(QWidget *parent = nullptr);
+
+    ~MainWindow() override;
 
 private slots:
+
     void on_loginButton_clicked();
+
     void loginResult(QNetworkReply *reply);
 
 private:
-
     Ui::MainWindow *ui;
-    QString loginurl = "https://localhost:5001/login";
+    QString loginUrl = "https://localhost:5001/login";
 };
-#endif // MAINWINDOW_H
+
+#endif // MAIN_WINDOW_H
