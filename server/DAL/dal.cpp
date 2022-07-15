03d8c7cfe0ea43110722f4bcb2a218af9730db26
kleroop <kleroop@protonmail.com>
2022-07-16 01:30:25 +0300

    User DAL initial structure

--- server/DAL/dal.cpp
+++ server/DAL/dal.cpp
@@ -1,7 +1,51 @@
 #include "dal.h"
-#include <stdio.h>
 
-void DALInit()
+#include <Poco/Data/PostgreSQL/Connector.h>
+#include <Poco/Data/Session.h>
+using namespace Poco::Data::PostgreSQL;
+using namespace Poco;
+
+// dumb but cpp can't static initialize object without constructing
+Data::Session *session_ = nullptr;
+#define session (*session_)
+
+DALStatus DALInit()
 {
-    puts("Inited");
+    Connector::registerConnector();
+
+    session_ = new Data::Session(Connector::KEY,
+                                 "host=localhost port=5433 user=admin password=admin dbname=test");
+
+    return DAL_OK;
+}
+
+void DALQuit()
+{
+    delete session_;
+}
+
+DALStatus DALUserCreate(DALUser *out, char *username, char *email, char *pwd_hash, bool is_admin)
+{
+    return DAL_OK;
+}
+
+DALStatus DALUserGetById(DALUser *out, uint32_t id)
+{
+    return DAL_OK;
+}
+
+DALStatus DALUserGetByUsername(DALUser *out, char *username)
+{
+    return DAL_OK;
+}
+
+DALStatus DALUserEdit(DALUser *out, DALUser uint32_t, char *username, char *email, char *pwd_hash,
+                      bool is_admin)
+{
+    return DAL_OK;
+}
+
+DALStatus DALUserDelete(uint32_t id)
+{
+    return DAL_OK;
 }
--- server/DAL/include/dal.h
+++ server/DAL/include/dal.h
@@ -2,12 +2,36 @@
 #define DAL_H
 
 #include <dal_export.h>
+#include <cstdint>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-DAL_EXPORT void DALInit();
+typedef enum { DAL_OK, DAL_USER_EXISTS } DALStatus;
+
+typedef struct
+{
+    int32_t id;
+    // max length defined by the standard + nul terminator
+    char username[32 + 1], email[320 + 1], pwd_hash[60 + 1];
+    bool is_admin;
+} DALUser;
+
+DAL_EXPORT DALStatus DALInit();
+
+DAL_EXPORT void DALQuit();
+
+DAL_EXPORT DALStatus DALUserCreate(DALUser *out, char username[32 + 1], char email[320 + 1],
+                                   char pwd_hash[60 + 1], bool is_admin);
+
+DAL_EXPORT DALStatus DALUserGetById(DALUser *out, uint32_t id);
+
+DAL_EXPORT DALStatus DALUserGetByUsername(DALUser *out, char username[32 + 1]);
+
+DAL_EXPORT DALStatus DALUserEdit(DALUser *inout);
+
+DAL_EXPORT DALStatus DALUserDelete(uint32_t id);
 
 #ifdef __cplusplus
 }
