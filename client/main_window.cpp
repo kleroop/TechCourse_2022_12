cc929929e35c9f7d37767d5462f796724101e8cf
romoho <roman.saviuk.knm.2020@lpnu.ua>
2022-07-21 23:32:43 +0300

    added todo

--- client/main_window.cpp
+++ client/main_window.cpp
@@ -6,14 +6,14 @@ using json = nlohmann::json;
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
-
-    ui->errorLabel->setText("");
+    //todo fix fonts
+    ui->errorLabel->setText(""); //todo delete with deleting sample text in ui.errorLabel when fonts fixed
 }
 
 
 void MainWindow::resizeEvent(QResizeEvent *event) {
     QWidget::resizeEvent(event);
-    double ratio = (double) 642 / (double) 1024;
+    double ratio = (double) 642 / (double) 1024;// todo replace hardcoded numbers with picture properties (QPixmap.width)
     int w = ratio * QWidget::size().height();
     ui->loginBgImg->setFixedWidth(w);
 }
@@ -25,7 +25,7 @@ MainWindow::~MainWindow() {
 
 
 void MainWindow::on_emailForm_textChanged() {
-    QString noErrorFormStyle("font-family: 'Open Sans';\n""font-style: normal;\n""font-weight: 400;\nfont-size: 14px;\n"
+    QString noErrorFormStyle("font-family: 'Open Sans';\n""font-style: normal;\n""font-weight: 400;\nfont-size: 14px;\n"//todo find better solution
                              "line-height: 19px;\n""padding-left: 15px;\n""border: 1px solid #414C5F;");
 
     ui->emailForm->setStyleSheet(noErrorFormStyle);
@@ -42,7 +42,7 @@ void MainWindow::on_passwordForm_textChanged() {
 void MainWindow::loginResult(QNetworkReply *reply) {
     json replyJson = toJson(reply);
 
-    QString errorFormStyle("font-family: 'Open Sans';\n""font-style: normal;\n""font-weight: 400;\nfont-size: 14px;\n"
+    QString errorFormStyle("font-family: 'Open Sans';\n""font-style: normal;\n""font-weight: 400;\nfont-size: 14px;\n" //todo find better solution
                            "line-height: 19px;\n""padding-left: 15px;\n""border: 1px solid #D72130;");
 
     if ((replyJson["status"]) != 200) {
@@ -56,7 +56,7 @@ void MainWindow::loginResult(QNetworkReply *reply) {
 }
 
 
-void MainWindow::on_loginBackButton_clicked() {
+void MainWindow::on_loginBackButton_clicked() { //todo delete with temporary page(tokenPage)
     ui->stackedWidget->setCurrentIndex(0);
 }
 
