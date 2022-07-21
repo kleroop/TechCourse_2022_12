983c89f1626f8b70749ba6a506a335dbb16788cf
romoho <roman.saviuk.knm.2020@lpnu.ua>
2022-07-21 22:59:00 +0300

    styles
    adaptive layout, fonts, stylesheets

--- client/main_window.cpp
+++ client/main_window.cpp
@@ -3,8 +3,26 @@
 
 using json = nlohmann::json;
 
+
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    ui->errorLabel->setText("");
+
+    QFontDatabase::addApplicationFont(R"(F:\SS\TechCourse_2022_12\client\Resources\fonts\open-sans\OpenSans-Bold.ttf)");
+    QFontDatabase::addApplicationFont(R"(F:\SS\TechCourse_2022_12\client\Resources\fonts\open-sans\OpenSans-Regular.ttf)");
+    QFontDatabase::addApplicationFont(R"(F:\SS\TechCourse_2022_12\client\Resources\fonts\open-sans\OpenSans-Semibold.ttf)");
+    QFontDatabase::addApplicationFont(R"(F:\SS\TechCourse_2022_12\client\Resources\fonts\roboto\RobotoCondensed-Bold.ttf)");
+    QFontDatabase::addApplicationFont(R"(F:\SS\TechCourse_2022_12\client\Resources\fonts\roboto\Roboto-Regular.ttf)");
+    QFontDatabase::addApplicationFont(R"(F:\SS\TechCourse_2022_12\client\Resources\fonts\roboto\Roboto-Medium.ttf)");
+}
+
+
+void MainWindow::resizeEvent(QResizeEvent *event) {
+    QWidget::resizeEvent(event);
+    double ratio = (double) 642 / (double) 1024;
+    int w = ratio * QWidget::size().height();
+    ui->loginBgImg->setFixedWidth(w);
 }
 
 
@@ -13,11 +31,31 @@ MainWindow::~MainWindow() {
 }
 
 
+void MainWindow::on_emailForm_textChanged() {
+    QString noErrorFormStyle("font-family: 'Open Sans';\n""font-style: normal;\n""font-weight: 400;\nfont-size: 14px;\n"
+                             "line-height: 19px;\n""padding-left: 15px;\n""border: 1px solid #414C5F;");
+
+    ui->emailForm->setStyleSheet(noErrorFormStyle);
+    ui->passwordForm->setStyleSheet(noErrorFormStyle);
+    ui->errorLabel->setText("");
+}
+
+
+void MainWindow::on_passwordForm_textChanged() {
+    MainWindow::on_emailForm_textChanged();
+}
+
+
 void MainWindow::loginResult(QNetworkReply *reply) {
     json replyJson = toJson(reply);
 
+    QString errorFormStyle("font-family: 'Open Sans';\n""font-style: normal;\n""font-weight: 400;\nfont-size: 14px;\n"
+                           "line-height: 19px;\n""padding-left: 15px;\n""border: 1px solid #D72130;");
+
     if ((replyJson["status"]) != 200) {
-        QMessageBox::information(this, "Connection Error", toQString(replyJson["data"]["message"]));
+        ui->errorLabel->setText(toQString(replyJson["data"]["message"]));
+        ui->emailForm->setStyleSheet(errorFormStyle);
+        ui->passwordForm->setStyleSheet(errorFormStyle);
     } else {
         ui->tokenText->setText(toQString(replyJson["data"]["token"]));
         ui->stackedWidget->setCurrentIndex(1);
--- client/main_window.h
+++ client/main_window.h
@@ -8,6 +8,7 @@
 #include <QNetworkReply>
 #include <QUrl>
 #include <QByteArray>
+#include "QFontDatabase"
 
 #include "models/api/auth_model.h"
 #include "utilsClient/nlohmanQt.h"
@@ -32,9 +33,15 @@ private slots:
 
     void on_loginBackButton_clicked();
 
+    void on_emailForm_textChanged();
+
+    void on_passwordForm_textChanged();
+
     void on_loginButton_clicked();
 
     void loginResult(QNetworkReply *reply);
+
+    void resizeEvent(QResizeEvent *event) override;
 
 private:
     Ui::MainWindow *ui;
