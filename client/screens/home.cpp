a92a42ae8dc24e4df5b093fa57b9cc34a4ff4793
kleroop <kleroop@protonmail.com>
2022-08-08 19:45:28 +0300

    Header done

--- client/screens/home.cpp
+++ client/screens/home.cpp
@@ -2,21 +2,23 @@
 #include "ui_Home.h"
 #include <user_profile.h>
 
-Home::Home(QWidget *parent) : QWidget(parent), ui(new Ui::Home) {
+Home::Home(QWidget *parent) : QWidget(parent), ui(new Ui::Home)
+{
     ui->setupUi(this);
 
     this->HeaderWidget = new Header(this);
+    auto prof = new UserProfile(this->HeaderWidget);
+    this->HeaderWidget->getRightSection()->addWidget(prof);
     delete ui->headerPlaceholder;
     ui->headerFrame->layout()->addWidget(HeaderWidget);
 }
 
-Home::~Home() {
+Home::~Home()
+{
     delete ui;
 }
 
-void Home::resizeEvent(QResizeEvent *event) {
+void Home::resizeEvent(QResizeEvent *event)
+{
     QWidget::resizeEvent(event);
 }
-
-
-
